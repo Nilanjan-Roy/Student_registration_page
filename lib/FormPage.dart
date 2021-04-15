7fbd2f09e11e6cfa6d3a33f414946c165d12814d
Nilanjan Roy <roynilanjan2000@gmail.com>
2021-04-15 20:00:47 +0530

    first commit

---
+++ lib/FormPage.dart
@@ -0,0 +1,246 @@
+import 'package:flutter/material.dart';
+import 'InputDeco_design.dart';
+
+class FormPage extends StatefulWidget {
+  @override
+  _FormPageState createState() => _FormPageState();
+}
+
+class _FormPageState extends State<FormPage> {
+  String name, email, phone;
+
+  //TextController to read text entered in text field
+  TextEditingController password = TextEditingController();
+  TextEditingController confirmpassword = TextEditingController();
+
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+
+  String valueChoose;
+  List listItem1 = [
+    "Item1",
+    "Item2",
+    "Item3",
+    "Item4",
+    "Item5",
+  ];
+
+  String valueChoose2;
+  List listItem2 = [
+    "A",
+    "B",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formkey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 70,
+                  child: Image.network(
+                      "https://protocodespoint.com/wp-content/uploads/2020/10/PROTO-CODERS-POINT-LOGO-water-mark-.png"),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.only(bottom: 15, left: 10, right: 10),
+                  child: TextFormField(
+                    keyboardType: TextInputType.text,
+                    decoration: buildInputDecoration(Icons.person, "Full Name"),
+                    validator: (String value) {
+                      if (value.isEmpty) {
+                        return 'Please Enter Name';
+                      }
+                      return null;
+                    },
+                    onSaved: (String value) {
+                      name = value;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.only(bottom: 15, left: 10, right: 10),
+                  child: TextFormField(
+                    keyboardType: TextInputType.text,
+                    decoration: buildInputDecoration(Icons.email, "Email"),
+                    validator: (String value) {
+                      if (value.isEmpty) {
+                        return 'Please a Enter Email';
+                      }
+                      if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
+                          .hasMatch(value)) {
+                        return 'Please a valid Email';
+                      }
+                      return null;
+                    },
+                    onSaved: (String value) {
+                      email = value;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.only(bottom: 15, left: 10, right: 10),
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: buildInputDecoration(Icons.phone, "Phone No"),
+                    validator: (String value) {
+                      if (value.isEmpty) {
+                        return 'Please enter phone no ';
+                      }
+                      return null;
+                    },
+                    onSaved: (String value) {
+                      phone = value;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.only(bottom: 15, left: 10, right: 10),
+                  child: Container(
+                    padding: EdgeInsets.only(left: 16, right: 16),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.blue),
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    child: DropdownButton(
+                      // icon: Icon(Icons.class__rounded),
+                      hint: Text('Select Stream'),
+                      dropdownColor: Colors.white,
+                      icon: Icon(Icons.arrow_drop_down),
+                      iconSize: 36,
+                      isExpanded: true,
+                      underline: SizedBox(),
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 22,
+                      ),
+                      value: valueChoose,
+                      onChanged: (newValue) {
+                        setState(() {
+                          valueChoose = newValue;
+                        });
+                      },
+                      items: listItem1.map((valueItem) {
+                        return DropdownMenuItem(
+                          value: valueItem,
+                          child: Text(valueItem),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.only(bottom: 15, left: 10, right: 10),
+                  child: Container(
+                    padding: EdgeInsets.only(left: 16, right: 16),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.blue),
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    child: DropdownButton(
+                      // icon: Icon(Icons.class__rounded),
+                      hint: Text('Select Section'),
+                      dropdownColor: Colors.white,
+                      icon: Icon(Icons.arrow_drop_down),
+                      iconSize: 36,
+                      isExpanded: true,
+                      underline: SizedBox(),
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 22,
+                      ),
+                      value: valueChoose2,
+                      onChanged: (newValue) {
+                        setState(() {
+                          valueChoose2 = newValue;
+                        });
+                      },
+                      items: listItem2.map((valueItem) {
+                        return DropdownMenuItem(
+                          value: valueItem,
+                          child: Text(valueItem),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.only(bottom: 15, left: 10, right: 10),
+                  child: TextFormField(
+                    controller: password,
+                    keyboardType: TextInputType.text,
+                    decoration: buildInputDecoration(Icons.lock, "Password"),
+                    validator: (String value) {
+                      if (value.isEmpty) {
+                        return 'Please a Enter Password';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.only(bottom: 15, left: 10, right: 10),
+                  child: TextFormField(
+                    controller: confirmpassword,
+                    obscureText: true,
+                    keyboardType: TextInputType.text,
+                    decoration:
+                        buildInputDecoration(Icons.lock, "Confirm Password"),
+                    validator: (String value) {
+                      if (value.isEmpty) {
+                        return 'Please re-enter password';
+                      }
+                      print(password.text);
+
+                      print(confirmpassword.text);
+
+                      if (password.text != confirmpassword.text) {
+                        return "Password does not match";
+                      }
+
+                      return null;
+                    },
+                  ),
+                ),
+                SizedBox(
+                  width: 200,
+                  height: 50,
+                  child: RaisedButton(
+                    color: Colors.redAccent,
+                    onPressed: () {
+                      if (_formkey.currentState.validate()) {
+                        print("successful");
+
+                        return;
+                      } else {
+                        print("UnSuccessfull");
+                      }
+                    },
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50.0),
+                        side: BorderSide(color: Colors.blue, width: 2)),
+                    textColor: Colors.white,
+                    child: Text("Submit"),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
